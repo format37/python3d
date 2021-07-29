917e83beb8c000eb384f15bcaddb55733f6de748
format37 <format37@gmail.com>
2021-07-29 07:16:09 +0300

    modified:   pymesh_examples/myplot.py 	modified:   pymesh_examples/pymesh_example_05.py

--- pymesh_examples/myplot.py
+++ pymesh_examples/myplot.py
@@ -1,12 +1,17 @@
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 
-def plot_mesh(your_mesh, filename = None):
-    # Create a new plot
-    figure = plt.figure()
-    axes = mplot3d.Axes3D(figure)
-    axes.add_collection3d(mplot3d.art3d.Poly3DCollection(your_mesh.vertices))    
-    # Show or save the plot
+def plot_vf(vertices, faces, colors, size_x=10, size_y=10, dpi=80, filename = None):
+
+    fig = plt.figure(figsize=(size_x, size_y), dpi=dpi)
+    ax = mplot3d.Axes3D(fig)
+
+    norm = plt.Normalize(colors.min(), colors.max())
+    colors = plt.cm.viridis(norm(colors))
+
+    pc = mplot3d.art3d.Poly3DCollection(vertices[faces], facecolors=colors, edgecolor="black")
+    ax.add_collection(pc)
+
     if filename is None:
         plt.show()
     else:
--- pymesh_examples/pymesh_example_05.py
+++ pymesh_examples/pymesh_example_05.py
@@ -1,5 +1,12 @@
 import pymesh
-from myplot import plot_mesh
+import numpy as np
+from myplot import plot_vf
 
-sponge = pymesh.meshio.load_mesh('/pymesh_examples/pymesh_example_04_3.stl')
-plot_mesh(sponge, '/pymesh_examples/pymesh_example_05_3_0.png')
+sponge = pymesh.meshio.load_mesh('pymesh_examples/pymesh_example_04_3.stl')
+
+vertices = sponge.vertices
+faces = sponge.faces
+
+colors = np.array([int(f*8/len(faces)) for f in range(len(faces))])
+
+plot_vf(vertices, faces, colors, filename = 'pymesh_example_04_3_0.png')
